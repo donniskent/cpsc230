fcf06a4c4d5a23c09003606cc88f47b204800225
donnis <donnis@debiancpsc230.com>
2021-01-18 15:47:46 -0500

    Exercise 1 pushed

---
+++ cpsc230/Exercise1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main() {
+	int user_number;
+	printf("Enter a number:");
+	scanf("%d",&user_number);
+
+	for(int i = user_number; i >= 0; i--) {
+	
+	printf("%d\n", i);
+
+
+}
+
+
+}
