7dc21916b01f5aca7a8c557bcc64861b2956fcd8
donnis <donnis@debiancpsc230.com>
2021-01-20 09:40:56 -0500

    Imported boolean library

--- cpsc230/Exercise2/utility.c
+++ cpsc230/Exercise2/utility.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 
 int userNumber(int min, int max) {
 	int num;
-	while(1 == 1){
+	while(true){
 	printf("Enter a number between %d and %d: ", min, max);
 	scanf("%d", &num);
 	if(num < min || num > max) {
